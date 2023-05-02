98cec90a92ed3d11381d57c77f7c26f4be2c6cc8
Matheus Labiuc <Matheuslabiuc@hotmail.com>
2023-05-01 21:07:54 -0300

    Add project files.

---
+++ Busca-CEP/Controllers/CepController.cs
@@ -0,0 +1,27 @@
+﻿using Busca_CEP.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Busca_CEP.Controllers
+{
+    public class CepController : Controller
+    {
+        private readonly ILogger<CepController> _logger;
+
+        public CepController(ILogger<CepController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
